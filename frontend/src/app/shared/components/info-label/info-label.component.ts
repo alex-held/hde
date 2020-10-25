852838552d3c1fb13e9e43d7f28fdeda5062e666
alex held <git@alexheld.io>
2020-10-25 09:32:10 +0100

    feat(contact): 💥 execute action on info-label click

--- frontend/src/app/shared/components/info-label/info-label.component.ts
+++ frontend/src/app/shared/components/info-label/info-label.component.ts
@@ -1,25 +1,34 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
-@Component ({
-	selector: 'app-info-label',
-	templateUrl: './info-label.component.html',
-	styleUrls: ['./info-label.component.sass']
+@Component({
+  selector: "app-info-label",
+  templateUrl: "./info-label.component.html",
+  styleUrls: ["./info-label.component.sass"]
 })
 export class InfoLabelComponent implements OnInit {
+  @Input()
+  title: string = "Title";
 
-	@Input ()
-	title: string = "Title"
+  @Input()
+  icon: string = "home";
 
-	@Input ()
-	icon: string = "home"
+  @Input()
+  value: string[] = ["Value"];
 
-	@Input ()
-	value: string = "Value"
+  @Input()
+  href: string | any;
 
-	constructor () {
-	}
+  @Input()
+  click: () => void = () => {
+    if (this.href) {
+      const url = this.href;
+      console.log("default click: " + this.title + "\nRouting to url: " + url);
+      window.open(url);
+    }
+    console.log("default click: " + this.title + "\nNop");
+  };
 
-	ngOnInit (): void {
-	}
+  constructor() {}
 
+  ngOnInit(): void {}
 }
